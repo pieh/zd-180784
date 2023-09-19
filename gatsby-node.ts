a3abbaf268822bc8b751cfc7f21ca13e579b4d7b
Michal Piechowiak <misiek.piechowiak@gmail.com>
2023-09-19 17:25:43 +0200

    add segfault-handler

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,5 +1,8 @@
 import type { GatsbyNode } from "gatsby";
 import { generatePreviewPages } from "./src/pageTemplates/previewPages";
+var SegfaultHandler = require("segfault-handler");
+
+SegfaultHandler.registerHandler("crash.log");
 
 export const createPages: GatsbyNode["createPages"] = async ({
   actions,
@@ -7,4 +10,8 @@ export const createPages: GatsbyNode["createPages"] = async ({
 }) => {
   const { createPage } = actions;
   generatePreviewPages(graphql, createPage);
+};
+
+export const createSchemaCustomization = () => {
+  // just to make sure this module is included in graphql-engine
 };
